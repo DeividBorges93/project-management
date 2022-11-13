f74295e7d041938053f5d14f8d383bbb8f0242e7
DeividBorges93 <deividmborges@gmail.com>
2022-11-13 17:30:26 -0300

    feat(route): :sparkles: cria rota de usuários
    chama a rota no server.js

--- backend/src/api/server.js
+++ backend/src/api/server.js
@@ -1,9 +1,13 @@
 require('dotenv/config');
 const middlewareError = require ('../middlewares/error.middleware');
+const { userRoutes } = require('../routes');
+
 
 const port = process.env.API_PORT;
 
 const api = require('./api');
+
+api.use(userRoutes);
 
 api.use(middlewareError);
 
