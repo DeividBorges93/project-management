5cff23246b896d0cf6d9f3125c5f158dda072212
DeividBorges93 <deividmborges@gmail.com>
2022-11-13 17:20:36 -0300

    feat(user): :sparkles: cria migration create-user

---
+++ backend/src/migrations/20221113190829-create-user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    })
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('Users');
+  },
+};
