1d0e2b4201a8803fe06701f9fdca122373b348f0
DeividBorges93 <deividmborges@gmail.com>
2022-11-13 17:11:49 -0300

    chore(error): :sparkles: adiciona o middleware de erro no server.js
    chama o middleware de erro com o api.use para capturar os erros junto com o express-async-errors

--- backend/src/api/server.js
+++ backend/src/api/server.js
@@ -1,8 +1,11 @@
 require('dotenv/config');
-
+const middlewareError = require ('../middlewares/error.middleware');
 
 const port = process.env.API_PORT;
+
 const api = require('./api');
+
+api.use(middlewareError);
 
 api.listen(port);
 console.log(`Api rodando na porta ${port}`);
