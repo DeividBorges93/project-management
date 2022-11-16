da29fc3624c9558a89e056b2264cb8552ef54780
DeividBorges93 <deividmborges@gmail.com>
2022-11-16 17:08:29 -0300

    feat(service): :sparkles: implementa project.service
    função de criar um novo projeto

---
+++ backend/src/services/project.service.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const projectSchemas = require('../schemas/project.schemas');
+const { Project, User } = require('../models');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(config.development);
+
+const createProject = async (req) => {
+  const { error } = projectSchemas.validate(req.body);
+  const { title, zipCode, deadline, cost, done } = req.body;
+  const { username } = req.headers;
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+    return { code, message };
+  };
+
+  const user = await User.findOne({ raw: true, where: { username }});
+
+  if (!user) return { code: 400, message: 'User not found' };
+
+  const result = sequelize.transaction(async (t) => {
+    const project = await Project.create({ title,
+      zipCode,
+      cost,
+      done,
+      deadline,
+      username
+    },
+    { transaction: t });
+
+    return project;
+  });
+
+  return result;
+}
+
+module.exports = {
+  createProject,
+};
