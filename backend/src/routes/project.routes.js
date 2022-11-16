a4923468958801589115e04d3c39d1c4f222875f
DeividBorges93 <deividmborges@gmail.com>
2022-11-16 19:43:38 -0300

    feat(route): :sparkles: cria rota GET /projects/:id

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 const auth = require('../middlewares/auth.middleware');
 
-const { createProject, getProjectByUsername } = require('../controllers/project.controller');
+const { createProject, getProjectByUsername, getProjectById } = require('../controllers/project.controller');
 
 router.post('/project', auth, createProject);
 
 router.get('/projects', auth, getProjectByUsername);
+router.get('/project/:id', auth, getProjectById);
 
 module.exports = router;
