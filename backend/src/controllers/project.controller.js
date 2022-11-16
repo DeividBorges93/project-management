da82b7b56520ff7d311f93281005e4b551207394
DeividBorges93 <deividmborges@gmail.com>
2022-11-16 17:33:20 -0300

    feat(controller): :sparkles: cria função getProjectByUsername() em projects.controller
    envia o req para projectService e entrega resposta com o resultado da chamada

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -11,6 +11,19 @@ const createProject = async (req, res, next) => {
   return res.status(201).json(result);
 };
 
+const getProjectByUsername = async (req, res, next) => {
+  const result = await projectService.getProjectByUsername(req);
+
+  if (result.code) {
+    next(result);
+    return;
+  };
+
+  return res.status(200).json(result);
+
+}
+
 module.exports = {
   createProject,
+  getProjectByUsername
 };
