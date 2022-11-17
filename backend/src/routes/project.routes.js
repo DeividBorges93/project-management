79ab8fa6557062aa0d8832ca25e451710f7c0f96
DeividBorges93 <deividmborges@gmail.com>
2022-11-17 13:23:23 -0300

    feat(route): :sparkles: implementa a rota PATCH /projects/:id/done

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 const auth = require('../middlewares/auth.middleware');
 
-const { createProject, getProjectByUsername, getProjectById, updateProject } = require('../controllers/project.controller');
+const {
+  createProject,
+  getProjectByUsername,
+  getProjectById,
+  updateProject,
+  updateDoneProject
+} = require('../controllers/project.controller');
 
 router.post('/project', auth, createProject);
 
@@ -12,5 +18,7 @@ router.get('/projects', auth, getProjectByUsername);
 router.get('/project/:id', auth, getProjectById);
 
 router.put('/projects/:id', auth, updateProject)
+
+router.patch('/projects/:id/done', auth, updateDoneProject)
 
 module.exports = router;
