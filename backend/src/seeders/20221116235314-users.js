472a261bbf239bdc16a38c24fc99a13f8e3b1b6f
DeividBorges93 <deividmborges@gmail.com>
2022-11-19 12:52:03 -0300

    fix(seed): :seedling: altera padrão de senha no seeder.

--- backend/src/seeders/20221116235314-users.js
+++ backend/src/seeders/20221116235314-users.js
@@ -4,14 +4,14 @@ module.exports = {
       'Users',
       [
         {
-          name: 'Deivid Borges',
-          password: 'minhasenhaaaaa',
+          name: 'deivid borges',
           username: 'deivid.borges',
+          password: '$2a$08$YEwZugbzj1Rp6cDICft5SOj5KwkwpGFq/wQ92vAb0F2Yl4UTMwxh.',
         },
         {
-          name: 'Deivid Machado',
-          password: 'minhasenhaaaaa',
+          name: 'deivid machado',
           username: 'deivid.machado',
+          password: '$2a$08$AIzDUCMBDX.bu6vkLSt./eDCBgwb0Kluz6FgChRx7jyvU3PZYixFa',
         },
       ],
 
