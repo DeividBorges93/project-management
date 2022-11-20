5c5e2e378180f2bfd779628557da23ca030d3bb8
DeividBorges93 <deividmborges@gmail.com>
2022-11-20 13:19:52 -0300

    feat(routes): :sparkles: cria rota /projects no frontend

--- project-manager-frontend/src/routes/index.routes.js
+++ project-manager-frontend/src/routes/index.routes.js
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import Login from '../pages/Login'
+import Projects from '../pages/ProjectsByUsername';
 import Register from '../pages/RegisterUser';
 
 function Router() {
@@ -9,6 +10,8 @@ function Router() {
         <Route element={ <Navigate to="/login" /> } exact path="/" />
         <Route element={ <Login /> } path="/login" />
         <Route element={ <Register /> } path="/register" />
+        <Route element={ <Projects /> } path="/projects" />
+
 
       </Routes>
     </BrowserRouter>
