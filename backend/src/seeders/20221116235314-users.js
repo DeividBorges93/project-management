fdb10161636d8b56713c956a08640f629693d93c
DeividBorges93 <deividmborges@gmail.com>
2022-11-20 13:27:59 -0300

    fix(seed): :seedling: altera formato da senha e deixa comentário com a senha digitada no input

--- backend/src/seeders/20221116235314-users.js
+++ backend/src/seeders/20221116235314-users.js
@@ -6,12 +6,12 @@ module.exports = {
         {
           name: 'deivid borges',
           username: 'deivid.borges',
-          password: '$2a$08$YEwZugbzj1Rp6cDICft5SOj5KwkwpGFq/wQ92vAb0F2Yl4UTMwxh.',
+          password: '$2a$08$YEwZugbzj1Rp6cDICft5SOj5KwkwpGFq/wQ92vAb0F2Yl4UTMwxh.', // minhasenha
         },
         {
           name: 'deivid machado',
           username: 'deivid.machado',
-          password: '$2a$08$AIzDUCMBDX.bu6vkLSt./eDCBgwb0Kluz6FgChRx7jyvU3PZYixFa',
+          password: '$2a$08$AIzDUCMBDX.bu6vkLSt./eDCBgwb0Kluz6FgChRx7jyvU3PZYixFa',// minhasenhaaa
         },
       ],
 
