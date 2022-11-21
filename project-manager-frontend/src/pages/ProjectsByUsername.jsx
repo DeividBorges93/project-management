e001188c68905b07450afcf0646aa929b3ef0d22
DeividBorges93 <deividmborges@gmail.com>
2022-11-21 14:46:14 -0300

    feat(components): :sparkles: cria componente que renderiza linha dos projetos

--- project-manager-frontend/src/pages/ProjectsByUsername.jsx
+++ project-manager-frontend/src/pages/ProjectsByUsername.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../style/projectsByUsername.page.css';
-
+import RowProject from '../components/RowProject';
 
 export default function ProjectsByUsername() {
   
@@ -20,33 +20,6 @@ export default function ProjectsByUsername() {
     .catch((err) => console.log(err.message));
     }, []);
 
-  const toogleCheckbox = (event) => {
-    const id = event.target.attributes[1].value
-    const doneCheck_url = `http://localhost:3001/projects/${id}/done`;
-
-    const data = {
-        done: true,
-      };
-    axios.patch(doneCheck_url, data, { headers: { username, Authorization } })
-      .then((response) => {
-        console.log(response.data);
-        document.location.reload()
-      })
-      .catch((err) => err.message)
-  }
-
-  const deleteProject = (event) => {
-    const id = event.target.attributes[1].value
-    const delete_url = `http://localhost:3001/projects/${id}`;
-
-    axios.delete(delete_url, { headers: { username, Authorization } })
-    .then((response) => {
-      console.log(response.data);
-      document.location.reload()
-    })
-      .catch((err) => err.message);
-  }
-
   return (
     <div className="container-up">
       <div className="container-uprojects">
@@ -59,25 +32,14 @@ export default function ProjectsByUsername() {
               <th>Título</th>
               <th>CEP</th>
               <th>Custo</th>
+              <th>Prazo</th>
               <th>Finalizado</th>
             </tr>
             </thead>
             <tbody>
-              {projects?.map((project, i) => {
-                const { cost, id, done, title, zipCode } = project;
-                return (
-                  <tr key={i}>
-                    <td>{id}</td>
-                    <td>{title}</td>
-                    <td>{zipCode}</td>
-                    <td>{cost}</td>
-                    <td>{done ? 'Finalizado' : 'Não finalizado'}</td>
-                    <td>{<button type='button' id={id} onClick={toogleCheckbox}>concluido</button>}</td>
-                    <td>{<button type='button' id={id} onClick={deleteProject}>deletar</button>}</td>
-
-                  </tr>
-                )
-              })}
+              {projects?.map((project, i) => (
+                <RowProject project={project} key={i} />
+              ))}
             </tbody>
           </table>
         </div>
