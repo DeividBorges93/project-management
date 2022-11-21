f4ac2a5fdb45dacc7bc157e2c48ab6ad03cad8dd
DeividBorges93 <deividmborges@gmail.com>
2022-11-21 13:06:59 -0300

    feat(pages): :sparkles: implenta lógica para deletar um projeto

--- project-manager-frontend/src/pages/ProjectsByUsername.jsx
+++ project-manager-frontend/src/pages/ProjectsByUsername.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState, Redirect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../style/projectsByUsername.page.css';
+
 
 export default function ProjectsByUsername() {
   
@@ -16,25 +18,41 @@ export default function ProjectsByUsername() {
       setProjects(response.data);
     })
     .catch((err) => console.log(err.message));
-    }, [username, Authorization]);
+    }, []);
 
   const toogleCheckbox = (event) => {
     const id = event.target.attributes[1].value
     const doneCheck_url = `http://localhost:3001/projects/${id}/done`;
+
     const data = {
         done: true,
       };
-      axios.patch(doneCheck_url, data, { headers: { username, Authorization } })
-      .then((response) => console.log(response.data))
+    axios.patch(doneCheck_url, data, { headers: { username, Authorization } })
+      .then((response) => {
+        console.log(response.data);
+        document.location.reload()
+      })
       .catch((err) => err.message)
   }
 
+  const deleteProject = (event) => {
+    const id = event.target.attributes[1].value
+    const delete_url = `http://localhost:3001/projects/${id}`;
+
+    axios.delete(delete_url, { headers: { username, Authorization } })
+    .then((response) => {
+      console.log(response.data);
+      document.location.reload()
+    })
+      .catch((err) => err.message);
+  }
+
   return (
-    <div className="container">
-      <div className="container-projects">
-        <div className="wrap-projects">
-          <h1 className='projects-title'>Projetos</h1>
-          <table>
+    <div className="container-up">
+      <div className="container-uprojects">
+        <div className="wrap-uprojects">
+          <h1 className='uprojects-title'>Projetos</h1>
+          <table className='uprojects-table'>
             <thead>
             <tr>
               <th>ID</th>
@@ -45,7 +63,7 @@ export default function ProjectsByUsername() {
             </tr>
             </thead>
             <tbody>
-              {projects.map((project, i) => {
+              {projects?.map((project, i) => {
                 const { cost, id, done, title, zipCode } = project;
                 return (
                   <tr key={i}>
@@ -55,6 +73,8 @@ export default function ProjectsByUsername() {
                     <td>{cost}</td>
                     <td>{done ? 'Finalizado' : 'Não finalizado'}</td>
                     <td>{<button type='button' id={id} onClick={toogleCheckbox}>concluido</button>}</td>
+                    <td>{<button type='button' id={id} onClick={deleteProject}>deletar</button>}</td>
+
                   </tr>
                 )
               })}
